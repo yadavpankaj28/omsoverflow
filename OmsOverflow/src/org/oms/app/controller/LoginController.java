f2e7afa2a54eb5516a8806a7a30c57ea45897cad
Pankaj Yadav <31302243+yadavpankaj28@users.noreply.github.com>
2019-02-04 17:40:04 +0530

    added hibernate file

--- OmsOverflow/src/org/oms/app/controller/LoginController.java
+++ OmsOverflow/src/org/oms/app/controller/LoginController.java
@@ -15,6 +15,8 @@ public class LoginController extends HttpServlet {
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		
+		
 	}
 
 }
