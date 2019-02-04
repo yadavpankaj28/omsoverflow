a692a7fff86c09031e93b056d2e818c7ab35302d
Pankaj Yadav <31302243+yadavpankaj28@users.noreply.github.com>
2019-02-04 17:10:39 +0530

    Initial Project Files

---
+++ OmsOverflow/src/org/oms/app/controller/LoginController.java
@@ -0,0 +1,20 @@
+package org.oms.app.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginController
+ */
+@WebServlet("/LoginController")
+public class LoginController extends HttpServlet {
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
